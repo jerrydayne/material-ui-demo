5aab83b94d106778cf31d1a31128849040994779
jerrydayne <jerrydaexplorer@gmail.com>
2024-08-16 22:06:55 +0100

    Typography

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,24 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant='h1'>Heading 1</Typography>
+      <Typography variant='h2'>Heading 2</Typography>
+      <Typography variant='h3'>Heading 3</Typography>
+      <Typography variant='h4'>Heading 4</Typography>
+
+      
+      <Typography variant='subtitle1'>subtitle-1</Typography>
+      <Typography variant='subtitle2'>subtitle-2</Typography>
+
+      <Typography variant='body1'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi, aut quasi eum dolores necessitatibus quidem esse aspernatur rerum inventore expedita asperiores officiis sequi, beatae, ab doloremque? Magni excepturi eligendi veritatis!
+      </Typography>
+      <Typography variant='body2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sapiente tenetur, natus sit at voluptas vitae veniam animi rerum quibusdam aliquam veritatis quo, facere tempore, debitis mollitia sint ratione laudantium?</Typography>
+    </div>
+  )
+}
+
+export default MuiTypography
