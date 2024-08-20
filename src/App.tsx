886abd2bae3bc8007e82ced1552095b6e1feb996
jerrydayne <jerrydaexplorer@gmail.com>
2024-08-20 23:16:12 +0100

    Autocomplete and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import MuiSwitch from './components/MuiSwitch';
-import MuiRating from './components/MuiRating';
+//import MuiSwitch from './components/MuiSwitch';
 //import MuiTypography from './components/MuiTypography';
 //import MuiButton from './components/MuiButton';
 //import MuiTextField from './components/MuiTextField';
 //import MuiSelect from './components/MuiSelect';
 //import MuiRadioButton from './components/MuiRadioButton';
 //import MuiCheckBox from './components/MuiCheckBox';
+//import MuiRating from './components/MuiRating';
+import MuiAutoComplete from './components/MuiAutoComplete';
 
 
 function App() {
@@ -20,7 +21,8 @@ function App() {
       {/**<MuiRadioButton /> */}
       {/**<MuiCheckBox /> */}
       {/**<MuiSwitch /> */}
-      <MuiRating />
+      {/**<MuiRating /> */}
+      <MuiAutoComplete />
     </div>
   );
 }
