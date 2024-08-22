96599adf0b8bf58711df7f4a5fb4e06e61ee5c1b
jerrydayne <jerrydaexplorer@gmail.com>
2024-08-22 21:04:02 +0100

    Grid basics and props

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,7 +1,8 @@
-import { Box, Divider, Stack } from "@mui/material"
+import { Box, Divider, Grid, Stack } from "@mui/material"
 
 const MuiLayout = () => {
   return (
+    <>
     <Stack
         spacing={2}
         direction='row'
@@ -33,6 +34,21 @@ const MuiLayout = () => {
         @jerrydayne
       </Box>
     </Stack>
+    <Grid container rowSpacing={2} columnSpacing={1} my={4}>
+        <Grid item xs={12} sm={6} lg='auto'>
+            <Box bgcolor='blue' p={2}>Box 1</Box>
+        </Grid>
+        <Grid item xs={12} sm={6} lg='auto'>
+            <Box bgcolor='blue' p={2}>Box 1</Box>
+        </Grid>
+        <Grid item xs={12} sm={6} lg='auto'>
+            <Box bgcolor='blue' p={2}>Box 1</Box>
+        </Grid>
+        <Grid item xs={12} sm={6} lg='auto'>
+            <Box bgcolor='blue' p={2}>Box 1</Box>
+        </Grid>
+    </Grid>
+    </>
   )
 }
 
